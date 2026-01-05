eb342d4ec21da47ff04803d2eb1dd3a025016c36
Seyed AmirMohammad Mirkarimi <mirkarimi.amirmohammad@gmail.com>
2026-01-05 20:17:35 +0330

    Set default PORT to 3000 if not specified

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('views', 'views');
 const server = http.createServer(app);
 const io = socketio(server);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
